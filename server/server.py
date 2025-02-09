717092404d7ebc92eb497ffa50b636b6e4252310
thibaut D <thibaut.delahaie@polymtl.ca>
2025-02-09 10:07:17 -0500

    adding server

--- server/server.py
+++ server/server.py
@@ -55,4 +55,4 @@ def list_planets():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000, ssl_context=('cert.pem', 'key.pem'))
